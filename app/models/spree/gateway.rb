3d1ebe5aab181321db2efe69c14d5305a92e4bdc
Nelson Kelem <nelson@21waves.com>
2013-05-01 11:20:37 +0100

    Changed the setting around in the model and engine.

--- app/models/spree/gateway.rb
+++ app/models/spree/gateway.rb
@@ -1,5 +1,6 @@
-module Spree::Gateway
-  def self.table_name_prefix
-    'spree_gateway_'
-  end
-end
+# module Spree::Gateway
+#   def self.table_name_prefix
+#     'spree_gateway_'
+#   end
+# end
+# Only useful if used in a gateway style.
--- lib/spree_gocardless/engine.rb
+++ lib/spree_gocardless/engine.rb
@@ -1,6 +1,7 @@
 module SpreeGocardless
   class Engine < Rails::Engine
     require 'spree/core'
+    require "gocardless"
     isolate_namespace Spree
     engine_name 'spree_gocardless'
 
@@ -11,18 +12,39 @@ module SpreeGocardless
       g.test_framework :rspec
     end
 
-    initializer "spree.gateway.payment_methods", after: "spree.register.payment_methods" do |app|
-      app.config.spree.payment_methods << Spree::Gateway::Gocardless
-    end
-
     def self.activate
-      # require "gocardless"
-
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+    end
 
-      # Gateway::Gocardless.register
+    initializer "spree.gateway.payment_methods", after: "spree.register.payment_methods" do |app|
+      app.config.spree.payment_methods << Spree::PaymentMethod::Gocardless
+
+      # Maybe there's a better way of doing this?
+
+      # The info below is provided from Spree Dashboard.
+
+      # Here's not the best place, since it's also run when executing rake commands!
+      # payment_method = Spree::PaymentMethod.find_by_name("GoCardless")
+
+      # if payment_method.preferred_test_mode and payment_method.preferred_server == 'test'
+      #   puts "Gocardless is in sandbox mode"
+      #   GoCardless.environment = :sandbox
+      # end
+      # if payment_method.preferred_test_mode and payment_method.preferred_server == 'test'
+      #   puts "Gocardless is in sandbox mode"
+      #   GoCardless.environment = :sandbox
+      # end
+
+      GoCardless.account_details = {
+        app_id:       GC_APP_ID,
+        app_secret:   GC_APP_SEKRET,
+        token:        GC_APP_TOKEN,
+        merchant_id:  GC_MERCHANT_ID
+      }
+
+      puts "GoCardless has been initialized"
     end
 
     config.to_prepare &method(:activate).to_proc
