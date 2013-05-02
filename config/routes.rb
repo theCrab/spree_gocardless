8c5385da6249a2004ae5c40d099ca8a762e28bbd
The Crab <nelson@safarista.com>
2013-05-02 13:15:33 +0100

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Spree::Core::Engine.routes.draw do
   # Add your extension routes here
-  match '/gocardless_callback' => 'gocardless#callback', :as => 'gocardless_callback', via => [:get]
-  match '/gocardless_webhook' => 'gocardless#webhook', :as => 'gocardless_webhook', via => [:post]
+  match '/gocardless_callback' => 'gocardless#callback', :as => 'gocardless_callback', :via => [:get]
+  match '/gocardless_webhook' => 'gocardless#webhook', :as => 'gocardless_webhook', :via => [:post]
 end
 
 # Spree::Core::Engine.routes.prepend do
